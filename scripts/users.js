20ad6cd041cedf4db27a39c8665aeb34110fa4ce
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-06-05 11:57:29 -0500

    made eric solutions on users.js but it's commented

--- scripts/users.js
+++ scripts/users.js
@@ -38,3 +38,69 @@ function makeDaTable(users) {
         tableBody.appendChild(row);
     });
 }
+
+// [ eric solution ]
+// HTML
+/*
+<table>
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>username</th>
+            <th>email</th>
+        </tr>
+    </thead>
+    <tbody id="userTbody">
+    </tbody>
+</table>
+*/
+// JavaScript
+/*
+
+window.onload = () => {
+    getAndDisplayUsers()
+}
+function getAndDisplayUsers(){
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+        .then((response) => response.json())
+        .then((users) => {
+            displayUsers(users)
+        })
+        .catch((error) => console.log("sh*t be broken. no users!"));
+}
+
+function displayUsers(users){
+    let tbody = document.querySelector("#userTbody");
+
+    for(let i = 0; i < users.length; i++){
+        let row = tbody.insertRow();
+
+        let cell1 = row.insertCell();
+        cell1.innerHTML = users[i].id
+        
+        let cell2 = row.insertCell();
+        cell2.innerHTML = users[i].name
+        
+        let cell3 = row.insertCell();
+        cell3.innerHTML = users[i].username
+
+        let cell4 = row.insertCell();
+        cell4.innerHTML = users[i].email
+
+        or
+
+        [ this is bad practice, the one above is better for security reasons/issues ]
+
+        tbody.innerHTML += `
+            <tr>
+                <td>${users[i].id}</td>
+                <td>${users[i].name}</td>
+                <td>${users[i].username}</td>
+                <td>${users[i].email}</td>
+            </tr>
+        `
+
+    }
+}
+*/
